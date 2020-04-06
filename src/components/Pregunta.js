3cb39ef281e14780df57b62cec05bb928a95d87e
Miguel Cobas <mcobas@dealergeek.com>
2020-04-05 21:28:26 -0500

    Se creo el componente de pregunta, y el componente de error que nos permite pasar el mensaje y reutilizarlo

--- src/components/Pregunta.js
+++ src/components/Pregunta.js
@@ -1,20 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import Error from './Error'
+
 
 const Pregunta = () => {
+
+    // Definir el state
+
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false); 
+
+    // Funcion de presupuesto
+
+    const definirPresupuesto = e => {
+        
+        guardarCantidad( parseInt(e.target.value,10))
+    }
+
+    const agregarPresupuesto = e =>{
+        e.preventDefault()
+        if(cantidad < 1 || isNaN( cantidad )){
+            guardarError(true)
+            return
+        }
+
+        guardarError(false)
+    }
+
+
   return (
     <Fragment>
       <h2>Coloca tu presupuesto</h2>
+      {error ? <Error mensaje = "El presupuesto es incorrecto"/>: null}
 
-      <form>
+      <form
+        onSubmit={agregarPresupuesto}
+      >
         <input
           type="number"
           className="u-full-width"
           placeholder="Coloca tu presupuesto"
+          onChange={definirPresupuesto}
         />
         <input
           type="submit"
           className="button primary u-full-width"
-          value="Definir presupuesto"
+          value="Definir Presupuesto"
         />
       </form>
     </Fragment>
