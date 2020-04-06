f6487266c90536a2222e79c215b2be6820764be6
Miguel Cobas <mcobas@dealergeek.com>
2020-04-05 23:09:25 -0500

    Se agrego el componente de Gastos y Gasto para poderlo enlistar en la segunda seccion

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Pregunta from "./components/Pregunta";
 import Formulario from "./components/Formulario";
+import Listado from "./components/Listado"
 
 function App() {
   // definir el state
@@ -36,7 +37,11 @@ function App() {
                   agregarNuevoGasto={agregarNuevoGasto}
                 />
               </div>
-              <div className="one-half column">2</div>
+              <div className="one-half column">
+                <Listado 
+                 gastos={gastos}
+                 />
+              </div>
             </div>
           )}
         </div>
